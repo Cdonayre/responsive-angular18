d9013562c027ed4cf77325e5838b2c17e6e4a9a5
Cdonayre <cdonayre91@gmail.com>
2025-07-02 16:37:01 -0500

    change color in menu items, sidebar and header background

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
-    //apiURL:'https://ms.dirisln.gob.pe:8080/api-docs/#',
-    apiURL:'https://app.dirislimanorte.gob.pe:8080/api',
+    apiURL:'https://ms.dirisln.gob.pe:8080/api',
+  //apiURL:'https://app.dirislimanorte.gob.pe:8080/api',
     apiUserUrl:'http://172.16.1.15:5000/api',
-    //apiGatewayUrl:'http://192.168.21.88:4000/api',
+  //apiGatewayUrl:'http://192.168.21.88:4000/api',
     apiGatewayUrl:'https://ms.dirisln.gob.pe/api',
     keyEncrypt: 'F5NMIGrCoz28',
 };
